af022dc9cdf6be5e44e47cb0a69cdea17fe99f04
Ariansyah Yutama <ariansyahy@yahoo.com>
2022-03-01 19:14:46 +0700

    revert for the az

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ data "aws_caller_identity" "current" {
 data "aws_region" "current" {
 }
 
-data "aws_availability_zones" "available" {
+data "aws_availability_zones" "all" {
   state = "available"
 }
 
